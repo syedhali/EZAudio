9f2b6b9c3e8d116a9b6a257b0a7457e0d1e539a6
Syed Haris Ali <syedhali07@gmail.com>
2014-02-16 12:25:05 -0800

    Adding EZAudioPlayer to EZAudio main includes

--- EZAudio/EZAudio.h
+++ EZAudio/EZAudio.h
@@ -35,6 +35,9 @@
 #import "EZOutput.h"
 #import "EZRecorder.h"
 
+#pragma mark - Extended Components
+#import "EZAudioPlayer.h"
+
 #pragma mark - Interface Components
 #import "EZPlot.h"
 #import "EZAudioPlot.h"
