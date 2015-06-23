32e27759d29892d14b2533dc9317d084f82c9034
Syed Haris Ali <syedhali07@gmail.com>
2015-06-23 11:13:02 -0700

    updated core graphics and openGL waveforms on iOS examples

--- EZAudioExamples/iOS/EZAudioCoreGraphicsWaveformExample/EZAudioCoreGraphicsWaveformExample/CoreGraphicsWaveformViewController.h
+++ EZAudioExamples/iOS/EZAudioCoreGraphicsWaveformExample/EZAudioCoreGraphicsWaveformExample/CoreGraphicsWaveformViewController.h
@@ -14,28 +14,62 @@
 /**
  We will allow this view controller to act as an EZMicrophoneDelegate. This is how we listen for the microphone callback.
  */
-@interface CoreGraphicsWaveformViewController : UIViewController <EZMicrophoneDelegate>
+@interface CoreGraphicsWaveformViewController : UIViewController <EZMicrophoneDelegate,
+                                                                  UIPickerViewDataSource,
+                                                                  UIPickerViewDelegate>
 
+//------------------------------------------------------------------------------
 #pragma mark - Components
+//------------------------------------------------------------------------------
+
 /**
  The CoreGraphics based audio plot
  */
-@property (nonatomic,weak) IBOutlet EZAudioPlot *audioPlot;
+@property (nonatomic, weak) IBOutlet EZAudioPlot *audioPlot;
 
 /**
  The microphone component
  */
-@property (nonatomic,strong) EZMicrophone *microphone;
+@property (nonatomic, strong) EZMicrophone *microphone;
 
+/**
+ The button at the bottom displaying the currently selected microphone input
+ */
+@property (nonatomic, weak) IBOutlet UIButton *microphoneInputToggleButton;
+
+/**
+ The microphone input picker view to display the different microphone input sources
+ */
+@property (nonatomic, weak) IBOutlet UIPickerView *microphoneInputPickerView;
+
+/**
+ The microphone input picker view's top layout constraint (we use this to hide the control)
+ */
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *microphoneInputPickerViewTopConstraint;
+
+/**
+ The text label displaying "Microphone On" or "Microphone Off"
+ */
+@property (nonatomic, weak) IBOutlet UILabel *microphoneTextLabel;
+
+
+//------------------------------------------------------------------------------
 #pragma mark - Actions
+//------------------------------------------------------------------------------
+
 /**
  Switches the plot drawing type between a buffer plot (visualizes the current stream of audio data from the update function) or a rolling plot (visualizes the audio data over time, this is the classic waveform look)
  */
--(IBAction)changePlotType:(id)sender;
+- (IBAction)changePlotType:(id)sender;
 
 /**
  Toggles the microphone on and off. When the microphone is on it will send its delegate (aka this view controller) the audio data in various ways (check out the EZMicrophoneDelegate documentation for more details);
  */
--(IBAction)toggleMicrophone:(id)sender;
+- (IBAction)toggleMicrophonePickerView:(id)sender;
+
+/**
+ Toggles the microphone on and off. When the microphone is on it will send its delegate (aka this view controller) the audio data in various ways (check out the EZMicrophoneDelegate documentation for more details);
+ */
+- (IBAction)toggleMicrophone:(id)sender;
 
 @end
