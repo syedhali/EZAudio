cdd1372e306d3c4ee64f3e2f98b43d3640f7fa2e
Syed Haris Ali <syedhali07@gmail.com>
2015-07-03 21:29:44 -0700

    added a ton of documentation

--- EZAudio/EZRecorder.h
+++ EZAudio/EZRecorder.h
@@ -60,11 +60,23 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 #pragma mark - EZRecorderDelegate
 //------------------------------------------------------------------------------
 
+/**
+ The EZRecorderDelegate for the EZRecorder provides a receiver for write events, `recorderUpdatedCurrentTime:`, and the close event, `recorderDidClose:`.
+ */
 @protocol EZRecorderDelegate <NSObject>
 
 @optional
 
+/**
+ Triggers when the EZRecorder is explicitly closed with the `closeAudioFile` method.
+ @param recorder The EZRecorder instance that triggered the action
+ */
 - (void)recorderDidClose:(EZRecorder *)recorder;
+
+/**
+ Triggers after the EZRecorder has successfully written audio data from the `appendDataFromBufferList:withBufferSize:` method.
+ @param recorder The EZRecorder instance that triggered the action
+ */
 - (void)recorderUpdatedCurrentTime:(EZRecorder *)recorder;
 
 @end
@@ -83,7 +95,7 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 //------------------------------------------------------------------------------
 
 /**
- <#Description#>
+ An EZRecorderDelegate to listen for the write and close events.
  */
 @property (nonatomic, weak) id<EZRecorderDelegate> delegate;
 
@@ -95,12 +107,27 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 /// @name Initializers
 ///-----------------------------------------------------------
 
+/**
+ Creates an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), and a file type (see `EZRecorderFileType`) that will automatically create an internal `fileFormat` and audio file type hint.
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileType A constant described by the EZRecorderFileType that corresponds to the type of destination file that should be written. For instance, an AAC file written using an '.m4a' extension would correspond to EZRecorderFileTypeM4A. See EZRecorderFileType for all the constants and mapping combinations.
+ @return A newly created EZRecorder instance.
+ */
 - (instancetype)initWithURL:(NSURL *)url
                clientFormat:(AudioStreamBasicDescription)clientFormat
                    fileType:(EZRecorderFileType)fileType;
 
 //------------------------------------------------------------------------------
 
+/**
+ Creates an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), and a file type (see `EZRecorderFileType`) that will automatically create an internal `fileFormat` and audio file type hint, as well as a delegate to respond to the recorder's write and close events.
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileType A constant described by the EZRecorderFileType that corresponds to the type of destination file that should be written. For instance, an AAC file written using an '.m4a' extension would correspond to EZRecorderFileTypeM4A. See EZRecorderFileType for all the constants and mapping combinations.
+ @param delegate        An EZRecorderDelegate to listen for the recorder's write and close events.
+ @return A newly created EZRecorder instance.
+ */
 - (instancetype)initWithURL:(NSURL *)url
                clientFormat:(AudioStreamBasicDescription)clientFormat
                    fileType:(EZRecorderFileType)fileType
@@ -108,6 +135,14 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 
 //------------------------------------------------------------------------------
 
+/**
+ Creates an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), a file format describing the destination format on disk (see `fileFormat` for more info), and an audio file type (an AudioFileTypeID for Core Audio, not a EZRecorderFileType).
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileFormat      An AudioStreamBasicDescription describing the format of the audio being written to disk (MP3, AAC, WAV, etc)
+ @param audioFileTypeID An AudioFileTypeID that matches your fileFormat (i.e. kAudioFileM4AType for an M4A format)
+ @return A newly created EZRecorder instance.
+ */
 - (instancetype)initWithURL:(NSURL *)url
                clientFormat:(AudioStreamBasicDescription)clientFormat
                  fileFormat:(AudioStreamBasicDescription)fileFormat
@@ -116,13 +151,13 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 //------------------------------------------------------------------------------
 
 /**
- <#Description#>
- @param url             <#url description#>
- @param clientFormat    <#clientFormat description#>
- @param fileFormat      <#fileFormat description#>
- @param audioFileTypeID <#audioFileTypeID description#>
- @param delegate        <#delegate description#>
- @return <#return value description#>
+ Creates an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), a file format describing the destination format on disk (see `fileFormat` for more info), an audio file type (an AudioFileTypeID for Core Audio, not a EZRecorderFileType), and delegate to respond to the recorder's write and close events.
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileFormat      An AudioStreamBasicDescription describing the format of the audio being written to disk (MP3, AAC, WAV, etc)
+ @param audioFileTypeID An AudioFileTypeID that matches your fileFormat (i.e. kAudioFileM4AType for an M4A format)
+ @param delegate        An EZRecorderDelegate to listen for the recorder's write and close events.
+ @return A newly created EZRecorder instance.
  */
 - (instancetype)initWithURL:(NSURL *)url
                clientFormat:(AudioStreamBasicDescription)clientFormat
@@ -153,12 +188,27 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 /// @name Class Initializers
 ///-----------------------------------------------------------
 
+/**
+ Class method to create an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), and a file type (see `EZRecorderFileType`) that will automatically create an internal `fileFormat` and audio file type hint.
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileType A constant described by the EZRecorderFileType that corresponds to the type of destination file that should be written. For instance, an AAC file written using an '.m4a' extension would correspond to EZRecorderFileTypeM4A. See EZRecorderFileType for all the constants and mapping combinations.
+ @return A newly created EZRecorder instance.
+ */
 + (instancetype)recorderWithURL:(NSURL *)url
                    clientFormat:(AudioStreamBasicDescription)clientFormat
                        fileType:(EZRecorderFileType)fileType;
 
 //------------------------------------------------------------------------------
 
+/**
+ Class method to create an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), and a file type (see `EZRecorderFileType`) that will automatically create an internal `fileFormat` and audio file type hint, as well as a delegate to respond to the recorder's write and close events.
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileType A constant described by the EZRecorderFileType that corresponds to the type of destination file that should be written. For instance, an AAC file written using an '.m4a' extension would correspond to EZRecorderFileTypeM4A. See EZRecorderFileType for all the constants and mapping combinations.
+ @param delegate        An EZRecorderDelegate to listen for the recorder's write and close events.
+ @return A newly created EZRecorder instance.
+ */
 + (instancetype)recorderWithURL:(NSURL *)url
                    clientFormat:(AudioStreamBasicDescription)clientFormat
                        fileType:(EZRecorderFileType)fileType
@@ -166,6 +216,14 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 
 //------------------------------------------------------------------------------
 
+/**
+ Class method to create an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), a file format describing the destination format on disk (see `fileFormat` for more info), and an audio file type (an AudioFileTypeID for Core Audio, not a EZRecorderFileType).
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileFormat      An AudioStreamBasicDescription describing the format of the audio being written to disk (MP3, AAC, WAV, etc)
+ @param audioFileTypeID An AudioFileTypeID that matches your fileFormat (i.e. kAudioFileM4AType for an M4A format)
+ @return A newly created EZRecorder instance.
+ */
 + (instancetype)recorderWithURL:(NSURL *)url
                    clientFormat:(AudioStreamBasicDescription)clientFormat
                      fileFormat:(AudioStreamBasicDescription)fileFormat
@@ -173,6 +231,15 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 
 //------------------------------------------------------------------------------
 
+/**
+ Class method to create an instance of the EZRecorder with a file path URL to write out the file to, a client format describing the in-application common format (see `clientFormat` for more info), a file format describing the destination format on disk (see `fileFormat` for more info), an audio file type (an AudioFileTypeID for Core Audio, not a EZRecorderFileType), and delegate to respond to the recorder's write and close events.
+ @param url             An NSURL representing the file path the output file should be written
+ @param clientFormat    An AudioStreamBasicDescription describing the in-applciation common format (always linear PCM)
+ @param fileFormat      An AudioStreamBasicDescription describing the format of the audio being written to disk (MP3, AAC, WAV, etc)
+ @param audioFileTypeID An AudioFileTypeID that matches your fileFormat (i.e. kAudioFileM4AType for an M4A format)
+ @param delegate        An EZRecorderDelegate to listen for the recorder's write and close events.
+ @return A newly created EZRecorder instance.
+ */
 + (instancetype)recorderWithURL:(NSURL *)url
                    clientFormat:(AudioStreamBasicDescription)clientFormat
                      fileFormat:(AudioStreamBasicDescription)fileFormat
@@ -184,7 +251,7 @@ typedef NS_ENUM(NSInteger, EZRecorderFileType)
 /**
  Class method to create a new instance of an EZRecorder using a destination file path URL and the source format of the incoming audio.
  @param url                 An NSURL specifying the file path location of where the audio file should be written to.
- @param sourceFormat        The AudioStreamBasicDescription for the incoming audio that will be written to the file.
+ @param sourceFormat        The AudioStreamBasicDescription for the incoming audio that will be written to the file (also called the `clientFormat`).
  @param destinationFileType A constant described by the EZRecorderFileType that corresponds to the type of destination file that should be written. For instance, an AAC file written using an '.m4a' extension would correspond to EZRecorderFileTypeM4A. See EZRecorderFileType for all the constants and mapping combinations.
  @return The newly created EZRecorder instance.
  */
