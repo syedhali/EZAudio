4b941078766025ca439524f6c3995934edfd278c
Syed Haris Ali <syedhali07@gmail.com>
2015-07-11 18:49:12 -0700

    updated examples

--- EZAudioExamples/OSX/EZAudioFFTExample/EZAudioFFTExample/FFTViewController.h
+++ EZAudioExamples/OSX/EZAudioFFTExample/EZAudioFFTExample/FFTViewController.h
@@ -31,29 +31,34 @@
 #import "EZAudio.h"
 
 /**
- Accelerate
- */
-#import <Accelerate/Accelerate.h>
-
-/**
  The FFTViewController demonstrates how to use the Accelerate framework to calculate the real-time FFT of audio data provided by an EZAudioMicrophone.
  */
-@interface FFTViewController : NSViewController <EZMicrophoneDelegate>
+@interface FFTViewController : NSViewController <EZMicrophoneDelegate, EZAudioFFTDelegate>
 
 #pragma mark - Components
 /**
  EZAudioPlot for frequency plot
  */
-@property (nonatomic,weak) IBOutlet EZAudioPlot *audioPlotFreq;
+@property (nonatomic, weak) IBOutlet EZAudioPlot *audioPlotFreq;
 
 /**
  EZAudioPlot for time plot
  */
-@property (nonatomic,weak) IBOutlet EZAudioPlot *audioPlotTime;
+@property (nonatomic, weak) IBOutlet EZAudioPlot *audioPlotTime;
 
 /**
- Microphone
+ A label used to display the maximum frequency (i.e. the frequency with the highest energy) calculated from the FFT.
  */
-@property (nonatomic,strong) EZMicrophone *microphone;
+@property (nonatomic, weak) IBOutlet NSTextField *maxFrequencyLabel;
+
+/**
+ The microphone used to get input.
+ */
+@property (nonatomic, strong) EZMicrophone *microphone;
+
+/**
+ Used to calculate a rolling FFT of the incoming audio data.
+ */
+@property (nonatomic, strong) EZAudioFFTRolling *fft;
 
 @end
--- EZAudioExamples/iOS/EZAudioFFTExample/EZAudioFFTExample/FFTViewController.m
+++ EZAudioExamples/iOS/EZAudioFFTExample/EZAudioFFTExample/FFTViewController.m
@@ -25,156 +25,93 @@
 
 #import "FFTViewController.h"
 
-@interface FFTViewController (){
-  COMPLEX_SPLIT _A;
-  FFTSetup      _FFTSetup;
-  BOOL          _isFFTSetup;
-  vDSP_Length   _log2n;
-}
-@end
+static vDSP_Length const FFTViewControllerFFTWindowSize = 4096;
 
 @implementation FFTViewController
-@synthesize audioPlotFreq;
-@synthesize audioPlotTime;
-@synthesize microphone;
 
+//------------------------------------------------------------------------------
 #pragma mark - Status Bar Style
+//------------------------------------------------------------------------------
+
 - (UIStatusBarStyle)preferredStatusBarStyle
 {
     return UIStatusBarStyleLightContent;
 }
 
-#pragma mark - Customize the Audio Plot
+//------------------------------------------------------------------------------
+#pragma mark - View Lifecycle
+//------------------------------------------------------------------------------
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    //
-    // Setup the AVAudioSession. EZMicrophone will not work properly on iOS
-    // if you don't do this!
-    //
-    AVAudioSession *session = [AVAudioSession sharedInstance];
-    NSError *error;
-    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&error];
-    if (error)
-    {
-        NSLog(@"Error setting up audio session category: %@", error.localizedDescription);
-    }
-    [session setActive:YES error:&error];
-    if (error)
-    {
-        NSLog(@"Error setting up audio session active: %@", error.localizedDescription);
-    }
     
-    /*
-    Customizing the audio plot's look
-    */
+    //
     // Setup time domain audio plot
-    self.audioPlotTime.backgroundColor = [UIColor colorWithRed: 0.569 green: 0.82 blue: 0.478 alpha: 1];
-    self.audioPlotTime.color           = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
-    self.audioPlotTime.shouldFill      = YES;
-    self.audioPlotTime.shouldMirror    = YES;
-    self.audioPlotTime.plotType        = EZPlotTypeRolling;
-
+    //
+    self.audioPlotTime.plotType = EZPlotTypeBuffer;
+    
+    //
     // Setup frequency domain audio plot
-    self.audioPlotFreq.backgroundColor = [UIColor colorWithRed: 0.984 green: 0.471 blue: 0.525 alpha: 1];
-    self.audioPlotFreq.color           = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
-    self.audioPlotFreq.shouldFill      = YES;
-    self.audioPlotFreq.plotType        = EZPlotTypeBuffer;
+    //
+    self.audioPlotFreq.shouldFill = YES;
+    self.audioPlotFreq.plotType = EZPlotTypeBuffer;
     self.audioPlotFreq.shouldCenterYAxis = NO;
-  
-    /*
-    Start the microphone
-    */
-    self.microphone = [EZMicrophone microphoneWithDelegate:self
-                                         startsImmediately:YES];
-  
+    
+    //
+    // Create an instance of the microphone and tell it to use this view controller instance as the delegate
+    //
+    self.microphone = [EZMicrophone microphoneWithDelegate:self];
+    
+    //
+    // Create an instance of the EZAudioFFTRolling to keep a history of the incoming audio data and calculate the FFT.
+    //
+    self.fft = [EZAudioFFTRolling fftWithWindowSize:FFTViewControllerFFTWindowSize
+                                         sampleRate:self.microphone.audioStreamBasicDescription.mSampleRate
+                                           delegate:self];
+    
+    //
+    // Start the mic
+    //
+    [self.microphone startFetchingAudio];
 }
 
-#pragma mark - FFT
-/**
- Adapted from http://batmobile.blogs.ilrt.org/fourier-transforms-on-an-iphone/
- */
--(void)createFFTWithBufferSize:(float)bufferSize withAudioData:(float*)data {
-  
-  // Setup the length
-  _log2n = log2f(bufferSize);
-  
-  // Calculate the weights array. This is a one-off operation.
-  _FFTSetup = vDSP_create_fftsetup(_log2n, FFT_RADIX2);
-  
-  // For an FFT, numSamples must be a power of 2, i.e. is always even
-  int nOver2 = bufferSize/2;
-  
-  // Populate *window with the values for a hamming window function
-  float *window = (float *)malloc(sizeof(float)*bufferSize);
-  vDSP_hamm_window(window, bufferSize, 0);
-  // Window the samples
-  vDSP_vmul(data, 1, window, 1, data, 1, bufferSize);
-  free(window);
-  
-  // Define complex buffer
-  _A.realp = (float *) malloc(nOver2*sizeof(float));
-  _A.imagp = (float *) malloc(nOver2*sizeof(float));
-  
-}
-
--(void)updateFFTWithBufferSize:(float)bufferSize withAudioData:(float*)data {
-  
-  // For an FFT, numSamples must be a power of 2, i.e. is always even
-  int nOver2 = bufferSize/2;
-  
-  // Pack samples:
-  // C(re) -> A[n], C(im) -> A[n+1]
-  vDSP_ctoz((COMPLEX*)data, 2, &_A, 1, nOver2);
-  
-  // Perform a forward FFT using fftSetup and A
-  // Results are returned in A
-  vDSP_fft_zrip(_FFTSetup, &_A, 1, _log2n, FFT_FORWARD);
-  
-  // Convert COMPLEX_SPLIT A result to magnitudes
-  float amp[nOver2];
-  float maxMag = 0;
-  
-  for(int i=0; i<nOver2; i++) {
-    // Calculate the magnitude
-    float mag = _A.realp[i]*_A.realp[i]+_A.imagp[i]*_A.imagp[i];
-    maxMag = mag > maxMag ? mag : maxMag;
-  }
-  for(int i=0; i<nOver2; i++) {
-    // Calculate the magnitude
-    float mag = _A.realp[i]*_A.realp[i]+_A.imagp[i]*_A.imagp[i];
-    // Bind the value to be less than 1.0 to fit in the graph
-    amp[i] = [EZAudioUtilities MAP:mag leftMin:0.0 leftMax:maxMag rightMin:0.0 rightMax:1.0];
-  }
-  
-  // Update the frequency domain plot
-  [self.audioPlotFreq updateBuffer:amp
-                    withBufferSize:nOver2];
-  
-}
-
+//------------------------------------------------------------------------------
 #pragma mark - EZMicrophoneDelegate
+//------------------------------------------------------------------------------
+
 -(void)    microphone:(EZMicrophone *)microphone
      hasAudioReceived:(float **)buffer
        withBufferSize:(UInt32)bufferSize
- withNumberOfChannels:(UInt32)numberOfChannels {
-  dispatch_async(dispatch_get_main_queue(), ^{
+ withNumberOfChannels:(UInt32)numberOfChannels
+{
+    //
+    // Calculate the FFT, will trigger EZAudioFFTDelegate
+    //
+    [self.fft computeFFTWithBuffer:buffer[0] withBufferSize:bufferSize];
     
-    // Update time domain plot
-    [self.audioPlotTime updateBuffer:buffer[0]
-                      withBufferSize:bufferSize];
-    
-    // Setup the FFT if it's not already setup
-    if( !_isFFTSetup ){
-      [self createFFTWithBufferSize:bufferSize withAudioData:buffer[0]];
-      _isFFTSetup = YES;
-    }
-    
-    // Get the FFT data
-    [self updateFFTWithBufferSize:bufferSize withAudioData:buffer[0]];
-    
-  });
+    __weak typeof (self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [weakSelf.audioPlotTime updateBuffer:buffer[0]
+                              withBufferSize:bufferSize];
+    });
 }
+
+//------------------------------------------------------------------------------
+#pragma mark - EZAudioFFTDelegate
+//------------------------------------------------------------------------------
+
+- (void)        fft:(EZAudioFFT *)fft
+ updatedWithFFTData:(float *)fftData
+         bufferSize:(vDSP_Length)bufferSize
+{
+    __weak typeof (self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        weakSelf.maxFrequencyLabel.text = [NSString stringWithFormat:@"Highest frequency: %.0f", [fft maxFrequency]];
+        [weakSelf.audioPlotFreq updateBuffer:fftData withBufferSize:(UInt32)bufferSize];
+    });
+}
+
+//------------------------------------------------------------------------------
 
 @end
