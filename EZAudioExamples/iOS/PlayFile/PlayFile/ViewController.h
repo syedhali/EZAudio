94eac1f2092ec117745efff321d9b97c17f70bcf
Syed Haris Ali <syedhali07@gmail.com>
2016-01-23 21:06:32 -0600

    Added new PlayFile example for iOS

---
+++ EZAudioExamples/iOS/PlayFile/PlayFile/ViewController.h
@@ -0,0 +1,115 @@
+//
+//  ViewController.h
+//  PlayFile
+//
+//  Created by Syed Haris Ali on 1/23/16.
+//  Copyright © 2016 Syed Haris Ali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//
+// First import the EZAudio header
+//
+#include <EZAudio/EZAudio.h>
+
+/**
+ Here's the default audio file included with the example
+ */
+#define kAudioFileDefault [[NSBundle mainBundle] pathForResource:@"simple-drum-beat" ofType:@"wav"]
+
+//------------------------------------------------------------------------------
+#pragma mark - ViewController
+//------------------------------------------------------------------------------
+
+@interface ViewController : UIViewController <EZAudioPlayerDelegate>
+
+//------------------------------------------------------------------------------
+#pragma mark - Components
+//------------------------------------------------------------------------------
+
+/**
+ An EZAudioFile that will be used to load the audio file at the file path specified
+ */
+@property (nonatomic, strong) EZAudioFile *audioFile;
+
+//------------------------------------------------------------------------------
+
+/**
+ An EZAudioPlayer that will be used for playback
+ */
+@property (nonatomic, strong) EZAudioPlayer *player;
+
+//------------------------------------------------------------------------------
+
+/**
+ The CoreGraphics based audio plot
+ */
+@property (nonatomic, weak) IBOutlet EZAudioPlotGL *audioPlot;
+
+//------------------------------------------------------------------------------
+
+/**
+ A label to display the current file path with the waveform shown
+ */
+@property (nonatomic, weak) IBOutlet UILabel *filePathLabel;
+
+//------------------------------------------------------------------------------
+
+/**
+ A slider to indicate the current frame position in the audio file
+ */
+@property (nonatomic, weak) IBOutlet UISlider *positionSlider;
+
+//------------------------------------------------------------------------------
+
+/**
+ A slider to indicate the rolling history length of the audio plot.
+ */
+@property (nonatomic, weak) IBOutlet UISlider *rollingHistorySlider;
+
+//------------------------------------------------------------------------------
+
+/**
+ A slider to indicate the volume on the audio player
+ */
+@property (nonatomic, weak) IBOutlet UISlider *volumeSlider;
+
+//------------------------------------------------------------------------------
+#pragma mark - Actions
+//------------------------------------------------------------------------------
+
+/**
+ Switches the plot drawing type between a buffer plot (visualizes the current stream of audio data from the update function) or a rolling plot (visualizes the audio data over time, this is the classic waveform look)
+ */
+- (IBAction)changePlotType:(id)sender;
+
+//------------------------------------------------------------------------------
+
+/**
+ Changes the length of the rolling history of the audio plot.
+ */
+- (IBAction)changeRollingHistoryLength:(id)sender;
+
+//------------------------------------------------------------------------------
+
+/**
+ Changes the volume of the audio player.
+ */
+- (IBAction)changeVolume:(id)sender;
+
+//------------------------------------------------------------------------------
+
+/**
+ Begins playback if a file is loaded. Pauses if the file is already playing.
+ */
+- (IBAction)play:(id)sender;
+
+//------------------------------------------------------------------------------
+
+/**
+ Seeks to a specific frame in the audio file.
+ */
+- (IBAction)seekToFrame:(id)sender;
+
+@end
